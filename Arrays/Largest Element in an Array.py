47cd100df6aecd4e824ce1c3f31de4de69190452
rekha-5 <167604595+rekha-5@users.noreply.github.com>
2026-07-06 23:01:49 +0530

    Update Largest Element in an Array.py

--- Arrays/Largest Element in an Array.py
+++ Arrays/Largest Element in an Array.py
@@ -1,27 +1,11 @@
-Largest Element in an Array
-Problem Description
-You are given an array of integers arr with size n. Your task is to find and return the largest element in the array.
-Note: The largest element is the integer with the greatest numerical value among all elements in the array.
-Difficulty
-Easy
-Example
-Input:
-text
-n = 5
-arr = [1, 8, 7, 56, 90]
-Output:
-text
-90
-Explanation:
-The largest element in the given array is 90.
-Constraints
-Approach
-Initialize a variable max_val with the first element of the array.
-Iterate through the array starting from the second element.
-If the current element is greater than max_val, update max_val.
-Return max_val after the loop finishes.
-Time Complexity: O(N)
-Space Complexity: O(1)
-
-
-
+class solution:
+    def largestElement(self, arr, n):
+        #Write your code here...
+        max_element=arr[0]
+        for i in range(1,n-1):
+            if(arr[i]>max_element):
+                max_element=arr[i]
+        return max_element
+        
+    
+       
